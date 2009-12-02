02851c6815185c7ba12e1d70da37fa056f20b20d
unknown <JCivinMini9@.(none)>
2009-12-02 12:27:41 -0800

    SVD preliminary.

--- svd/SVDBuilder.java
+++ svd/SVDBuilder.java
@@ -1,9 +1,56 @@
 package svd;
 
+import movieRatings.MovieID_Ratings;
+import movieRatings.UserRating;
+import neustore.base.LRUBuffer;
+
+import java.io.*;
+import java.lang.Object;
+import java.util.ArrayList;
+
+import cern.colt.PersistentObject;
+
 public class SVDBuilder {
-	
-	
-	public SVDBuilder(String neustoreTrainingSetFile) {
+
+	/*
+	 * rows = movieID
+	 * cols = userID
+	 * 
+	 * matrix=> rows X cols = m X n = movieID X userID
+	 */
+	private int rows, cols;
 		
+	//public SVDBuilder(String neustoreTrainingSetFile) {
+	public SVDBuilder(File index, String outputFile) {
+		
+		//DoubleFactory2D svdfactory;
+		//svdfactory = DoubleFactory2D.sparse;
+		//DoubleMatrix2D svdbuild = factory.make(rows, cols);
+		
+		double[][] matrix = new double[rows][cols];
+		double temp;
+		
+		MovieID_Ratings theRatings = new MovieID_Ratings(new LRUBuffer(5, 4096), index.getAbsolutePath(), 0);
+		ArrayList<UserRating> ratings;
+		
+		for(int row=0; row<rows; ++row) {
+			ratings = theRatings.getRatingsById(row+1);
+			for(int col=0; col<cols; ++col){
+				//svdbuild.setQuick(row,col, (double)ratings.get(col).rating);
+				temp = (double)ratings.get(col).rating;
+				
+				if(temp != null)
+					matrix[row][col] = temp;
+			}
+		}
+		
+		SparseDoubleMatrix2D svdbuild = new SparseDoubleMatrix2D(matrix);
+		svdbuild.trimToSize();
+		
+		SingularValueDecomposition svdTab = new SingularValueDecomposition(svdbuild);
+		
+		ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(outputFile));
+		os.writeObject(svdTab);
+		os.close();
 	}
 }
--- svd/SVDPredictor.java
+++ svd/SVDPredictor.java
@@ -1,5 +1,11 @@
 package svd;
 
-public class SVDPredictor {
+import tortuga.Predictor;
+import tortuga.PredictionIO;
+
+public class SVDPredictor implements Predictor{
 	/* instantiated by passing in an index file, has a float predictRating(int movieid, int userid) */
+	public SVDPredictor(File index, String svdFile){
+		
+	}
 }
