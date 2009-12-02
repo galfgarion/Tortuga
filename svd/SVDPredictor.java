86066de07b9dd5cf60399cacdb4a82051ff4ffb9
unknown <JCivinMini9@.(none)>
2009-12-02 13:30:27 -0800

    Preliminary work on Predictor.

--- svd/SVDPredictor.java
+++ svd/SVDPredictor.java
@@ -3,9 +3,30 @@ package svd;
 import tortuga.Predictor;
 import tortuga.PredictionIO;
 
+import java.io.*;
+
+import cern.colt.matrix.impl.*;
+import cern.colt.matrix.linalg.*;
+
 public class SVDPredictor implements Predictor{
 	/* instantiated by passing in an index file, has a float predictRating(int movieid, int userid) */
-	public SVDPredictor(File index, String svdFile){
-		
+	
+	private SingularValueDecomposition svd;
+	
+	public SVDPredictor(File index, String svdFile) throws IOException, ClassNotFoundException {
+		try{
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(svdFile));
+			svd = (SingularValueDecomposition)ois.readObject();
+			
+			ois.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public float predictRating(int movieID, int userID){
+		float result = 0;
+		return result;
 	}
 }
